d9971863a99ceec2d01c6fd5c3dc67f453638227
anna <anna.jablonska18@gmail.com>
2022-06-19 18:57:22 +0200

    ify

--- simple_roller_coster.py
+++ simple_roller_coster.py
@@ -1,16 +1,24 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+bill = 0
 min_height_in_cm = 120
 
 if height >= min_height_in_cm:
     print("Your can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Children tickets are $5.")
     elif age <= 18:
-        print("Please pay $7")
+        bill = 7
+        print("Youth tickets are $7")
     else:
-        print("Please pay $12")
+        bill = 12
+        print("Adult tickets are $12")
+    photo = input("Do you want photo taken? Y or N ")
+    if photo == "Y":
+        bill += 3
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride")
