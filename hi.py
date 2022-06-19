0a94e9a9e5ba4b6aedfe14c6b4af554285cea8b6
anna <anna.jablonska18@gmail.com>
2022-06-19 10:07:40 +0200

    Lol2

--- hi.py
+++ hi.py
@@ -1,5 +1,5 @@
 print("Welcome to the band generator")
-city = input("Which city did you grew uo in?")
-pet = input("What is the name of a pet? ")
+city = input("Which city did you grew uo in?\n")
+pet = input("What is the name of a pet?\n")
 print(f"Your band name could be {city} {pet}")
 
