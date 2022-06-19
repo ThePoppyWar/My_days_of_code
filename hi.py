fec37267c3d1c83107fbfcf82550b391d474fee4
anna <anna.jablonska18@gmail.com>
2022-06-19 10:06:43 +0200

    Lol

--- hi.py
+++ hi.py
@@ -1,1 +1,5 @@
-print("hello word")
+print("Welcome to the band generator")
+city = input("Which city did you grew uo in?")
+pet = input("What is the name of a pet? ")
+print(f"Your band name could be {city} {pet}")
+
