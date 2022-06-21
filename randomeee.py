3e124f104084594eb630330bc402e997102b229d
anna <anna.jablonska18@gmail.com>
2022-06-21 22:08:08 +0200

    randomy2

--- randomeee.py
+++ randomeee.py
@@ -1,6 +1,5 @@
 import random
 
-
 gamer = input("Choise: Heads or Tails ").lower()
 
 computer = random.randint(0, 1)
@@ -9,3 +8,11 @@ if computer == 1:
     print("Heads")
 else:
     print("Tails")
+
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+index_names = random.randint(0, (len(names) - 1))
+print(f"{names[index_names]} is going to buy the meal today!")
+
