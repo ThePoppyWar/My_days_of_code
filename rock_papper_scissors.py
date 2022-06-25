d672e5075994b4aefe5f73d5608fef73e31d0bfc
anna <anna.jablonska18@gmail.com>
2022-06-25 09:34:04 +0200

    my_version2

--- rock_papper_scissors.py
+++ rock_papper_scissors.py
@@ -26,36 +26,28 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+games_images = [rock, paper, scissors]
 gamer = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+print(games_images[gamer])
 
 computer = random.randint(0, 2)
-print(f"Computer chose {computer}")
+print(f"Computer chose:")
+print(games_images[computer])
+
 
 if gamer == computer:
     print("It's a draw")
 elif ((gamer == 0) and (computer == 2)):
-    print(rock)
-    print(scissors)
     print("You lose!")
 elif gamer == 2 and computer == 0:
-    print(scissors)
-    print(rock)
     print("You win!")
 elif gamer == 0 and computer == 1:
-    print(rock)
-    print(paper)
     print("You win!")
 elif gamer == 1 and computer == 0:
-    print(paper)
-    print(rock)
     print("You lose!")
 elif gamer == 2 and computer == 1:
-    print(scissors)
-    print(paper)
     print("You win!")
 elif gamer == 1 and computer == 2:
-    print(paper)
-    print(scissors)
     print("You lose!")
 elif gamer >= 2 or gamer < 0:
     print("You typed in invalid number, you lose!")
