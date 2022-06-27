c4b05f73f22bc29815af6680644b229fa84e7833
anna <anna.jablonska18@gmail.com>
2022-06-27 21:06:22 +0200

    binerny

--- OOP/palindrome.py
+++ OOP/palindrome.py
@@ -1,8 +1,8 @@
 def is_palindrome(number):
     numer_str = str(number)
-    if numer_str == numer_str[::-1]:
-        return True
-    else:
+    if numer_str != numer_str[::-1]:
         return False
+    bin_number = bin(number)[2:]
+    return bin_number == bin_number[::-1]
 
 print(is_palindrome(363))
