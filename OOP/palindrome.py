5c6f37f0cd4821f8304f14906652d78e2f915a3f
anna <anna.jablonska18@gmail.com>
2022-06-28 19:39:30 +0200

    trzy

--- OOP/palindrome.py
+++ OOP/palindrome.py
@@ -5,4 +5,25 @@ def is_palindrome(number):
     bin_number = bin(number)[2:]
     return bin_number == bin_number[::-1]
 
-print(is_palindrome(363))
+
+print(is_palindrome(99))
+
+
+def palindrome_three_numbers():
+    numbers = list()
+    for i in range(100, 1000):
+        if is_palindrome(i) == True:
+            numbers.append(i)
+    return numbers
+
+
+print(palindrome_three_numbers())
+
+
+# or
+
+def find_three_palindrome():
+    return [i for i in range(100, 1000) if is_palindrome(i)]
+
+
+print(find_three_palindrome())
