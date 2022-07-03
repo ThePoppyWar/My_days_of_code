8d0746bcb856b0978f2e85ca13b5cbf3f5e16fef
anna <anna.jablonska18@gmail.com>
2022-07-03 10:40:05 +0200

    cities

--- Grading_program.py
+++ Grading_program.py
@@ -18,7 +18,41 @@ for key in student_scores:
     else:
         student_grades[key] = "Fail"
 
-
-
-
 print(student_grades)
+
+# Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# travel_log = {
+#     {"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     {"country": "German", "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
+
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+
+def add_new_country(country, visit_time, cities: list):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visit_time
+    new_country["cities"] = cities
+    travel_log.append(new_country)
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
