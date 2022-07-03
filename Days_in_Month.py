aa9baabe1e0ad71085971bd9f927c7bcd18eb412
anna <anna.jablonska18@gmail.com>
2022-07-03 17:13:13 +0200

    days

--- Days_in_Month.py
+++ Days_in_Month.py
@@ -11,14 +11,25 @@ def is_leap(year):
         return False
 
 
+# def days_in_month(year, month):
+#     if month > 12 or month < 1:
+#         return "Invalid month1"
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     for _ in month_days:
+#         if is_leap(year) == False:
+#             return month_days[month - 1]
+#         if is_leap(year) == True:
+#             month_days[1] = 29
+#             return month_days[month - 1]
+
 def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month1"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    for _ in month_days:
-        if is_leap(year) == False:
-            return month_days[month - 1]
-        if is_leap(year) == True:
-            month_days[1] = 29
-            return month_days[month - 1]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
