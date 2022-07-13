264bf7603ce1c7184fe8a5992be9811daaf82e3d
anna <anna.jablonska18@gmail.com>
2022-07-13 21:02:23 +0200

    part2_coffe_machine

--- Coffee_machine/main_code.py
+++ Coffee_machine/main_code.py
@@ -2,4 +2,25 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+is_on = True
+money = MoneyMachine()
+maker_coffee = CoffeeMaker()
+menu = Menu()
+
+
+maker_coffee.report()
+money.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        maker_coffee.report()
+        money.report()
+    else:
+        order = menu.find_drink(choice)
+        print(order)
+
 
