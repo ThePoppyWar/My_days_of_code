cd916e566ef5e2ab031aa77a5c356f6932ad4fbf
anna <anna.jablonska18@gmail.com>
2022-08-07 11:37:04 +0200

    turtle5

--- Turtle_challenge/random_walk.py
+++ Turtle_challenge/random_walk.py
@@ -1,18 +1,24 @@
 import random
 import turtle as t
 
-
 tim = t.Turtle()
-colors = ['light gray', 'dark green', "teal", 'dark magenta', "medium purple", "gold"]
+t.colormode(255)
+
+
+def random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    random_color = (red, green, blue)
+    return random_color
+
+
 diractions = [0, 100, 90, 180, 250]
 
 tim.pensize(15)
-tim.speed("slowest")
-
-
-
+tim.speed("slow")
 
 for _ in range(300):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(diractions))
