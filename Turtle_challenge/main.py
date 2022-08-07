589b076db02515b339e3224aae667952a1876418
anna <anna.jablonska18@gmail.com>
2022-08-07 10:43:50 +0200

    turtle-first

--- Turtle_challenge/main.py
+++ Turtle_challenge/main.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('darkblue')
+tim = Turtle()
+# timmy_the_turtle.shape('turtle')
+# timmy_the_turtle.color('darkblue')
+# timmy_the_turtle.forward(150)
+# timmy_the_turtle.right(40)
 
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
 
 screen = Screen()
 screen.exitonclick()
