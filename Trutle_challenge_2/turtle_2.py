19638f9cd2204376116ebf6601dc43d79d5ca943
anna <anna.jablonska18@gmail.com>
2022-08-07 18:59:35 +0200

    snake_first

--- Trutle_challenge_2/turtle_2.py
+++ Trutle_challenge_2/turtle_2.py
@@ -1,7 +1,7 @@
 import random
 from turtle import Turtle, Screen
 
-is_race_on = False
+
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
@@ -15,6 +15,8 @@ for turtle_index in range(0, 6):
     new_turtle.color(colors[turtle_index])
     new_turtle.goto(x=-230, y=y_position[turtle_index])
     all_turtles.append(new_turtle)
+
+is_race_on = False
 
 if user_bet:
     is_race_on = True
