0f577782826a9e014bf26255990fcb2e5b0a8a1a
anna <anna.jablonska18@gmail.com>
2022-08-07 16:40:57 +0200

    screen

---
+++ Trutle_challenge_2/screen.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def move_right():
+    tim.right(10)
+
+
+def move_left():
+    tim.left(10)
+
+
+screen.listen()
+screen.onkey(move_forwards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(move_right, 'a')
+screen.onkey(move_left, 'd')
+screen.exitonclick()
