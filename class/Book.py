f96495ff19abfd13fa7a8d6fbf83914547912999
anna <anna.jablonska18@gmail.com>
2022-08-23 18:40:23 +0200

    titles

--- class/Book.py
+++ class/Book.py
@@ -1,3 +1,6 @@
+from turtle import title
+
+
 class Book:
 
     language = 'ENG'
@@ -21,3 +24,37 @@ book1.author = "Stanisław Len"
 print(book2.author, book1.author)
 
 print(book1.__dict__, book2.__dict__)
+
+del book1.author
+
+print(book1.__dict__, book2.__dict__)
+print(book2.author, book1.author)
+
+book1.title = 'Pan Tadeusz'
+book2.year = 1995
+print(book1.__dict__, book2.__dict__)
+print("------------------------------------------------------Book 1--------------------------------")
+class Book1:
+
+    language = 'ENG'
+    author = 'Mickiewicz'
+
+print(Book1.__dict__)
+
+books = [Book1(), Book1(), Book1()]
+print(books)
+
+for book in books:
+    print(f"jezyk: {book.language}, author: {book.author}")
+
+print("------------------------------------------------------Book titles--------------------------------")
+
+titles = ["Sonety krumskie", 'Pan Tadeusz', 'Konrad Wallenrod']
+
+
+for book, value in zip(books, titles):
+    book.title = value
+
+for book in books:
+    print(f"jezyk: {book.language}, author: {book.author}, tytuł: {book.title}")
+
